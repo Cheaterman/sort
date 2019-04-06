20f8cc73f502c9e452dbaf0da1e8f7142e92bc91
The Cheaterman <the.cheaterman@gmail.com>
2019-04-06 18:43:59 +0200

    Make bsort have deterministic number of iterations

--- bsort.py
+++ bsort.py
@@ -4,13 +4,9 @@ def bsort(iterable):
     return _bsort(list(iterable))
 
 def _bsort(iterable):
-    while True:
-        swapped = False
-
-        for index in range(0, len(iterable) - 1, 1):
+    for offset in range(0, len(iterable) - 1):
+        for index in range(0, len(iterable) - 1 - offset):
             if iterable[index] > iterable[index + 1]:
                 swap(iterable, index, index + 1)
-                swapped = True
 
-        if not swapped:
-            return iterable
+    return iterable
