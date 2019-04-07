0c67a435a7f29302fe3269d4a8b72c09e2a817f9
The Cheaterman <the.cheaterman@gmail.com>
2019-04-07 12:50:46 +0200

    Make msort iterative instead of recursive - also improve performance

--- msort.py
+++ msort.py
@@ -1,24 +1,44 @@
+from collections import deque
+from itertools import islice
+
+
 def msort(iterable):
-    return _msort(list(iterable))
+    return _msort(deque(iterable))
 
 def _msort(iterable):
-    if len(iterable) <= 1:
-        return iterable
+    msort_stack = deque([iterable])
+    merge_stack = deque()
 
-    middle = len(iterable) // 2
+    while True:
+        if msort_stack:
+            iterable = msort_stack.popleft()
 
-    left, right = _msort(iterable[:middle]), _msort(iterable[middle:])
+            middle = len(iterable) // 2
+            left, right = (
+                deque(islice(iterable, 0, middle)),
+                deque(islice(iterable, middle, len(iterable))),
+            )
 
-    return merge(left, right)
+            if all(len(side) <= 1 for side in (left, right)):
+                merge_stack.extend([left, right])
+            else:
+                msort_stack.extend([left, right])
+
+        elif len(merge_stack) > 1:
+            left, right = merge_stack.popleft(), merge_stack.popleft()
+            merge_stack.append(merge(left, right))
+
+        else:
+            return list(merge_stack.pop())
 
 def merge(left, right):
-    result = []
+    result = deque()
 
     while left and right:
         if left[0] > right[0]:
-            result.append(right.pop(0))
+            result.append(right.popleft())
         else:
-            result.append(left.pop(0))
+            result.append(left.popleft())
 
     result.extend(left + right)
 
